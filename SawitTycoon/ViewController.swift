d1e83db6fa754fb90a984de78450c7579320b305
dhamaddam <dhamaddam@gmail.com>
2026-08-21 21:38:44 +0700

    Initial Commit

---
+++ SawitTycoon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SawitTycoon
+//
+//  Created by Pusat Penelitian Kelapa Sawit on 21/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
